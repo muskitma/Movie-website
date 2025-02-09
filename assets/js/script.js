07739bfe33ee64d3a5dd497f4998a5be0fa37e80
muskitma <tm.ahamedmuski@gmail.com>
2025-02-09 20:05:03 +0530

    up

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * navbar variables
+ */
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+  });
+
+}
+
+
+
+/**
+ * header sticky
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
+document.querySelectorAll('.movie-card').forEach(card => {
+  card.style.display = 'block';
+});
+setTimeout(() => {
+    let lastMovie = document.querySelector('.movies-list li:last-child');
+    if (lastMovie) lastMovie.style.display = 'block';
+}, 1000);
